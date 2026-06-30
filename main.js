02580163c8c13dfdeb7efd17fede5905f3ba1397
anni080 <silva.anni@escola.pr.gov.br>
2026-06-30 11:36:27 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;1 <botoes.length;i++){
+     botoes[i].onclick = function(){
+
+        for(let j=0;j<botoes.length;j++){
+             botoes[j].classList.remove("ativo");
+}
+botoes[i].classList.add("ativo");
+     }
+        }
+           
+        const contadores= document.querySelectorAll(".contador");
+        const tempoObjetivo1= new Date("2024-10-05T00:00:00");
+        const tempoObjetivo2= new Date("2024-12-05T00:00:00");
+        const tempoObjetivo3= new Date("2024-12-30T00:00:00");
+        const tempoObjetivo4= new Date("2025-02-01T00:00:00");
+
+        const tempos= [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+        function calculaTempo(tempoObjeivo){
+          let tempoAtual= new Date();
+          let tempoFinal= tempoObjtivo-tempoAtual;
+          let segundos= Math.floor(tempoFinal/1000);
+          let minutos= Math.floor(segundos/60);
+          let horas= Math.floor(minutos/60);
+          let dias= Math.floor(horas/24);
+
+          segundos%=60;
+          minutos%=60;
+          horas%=24
+          if(tempoFinal>0){
+               return[dias,horas,minutos,segundos];
+
+          }else{
+               return[0,0,0,0];
+          }
+        }
+
